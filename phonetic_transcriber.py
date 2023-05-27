401216aaed6a5838f2b9a2a6fb86e92936225005
Didzis Gosko <didzis@users.noreply.github.com>
2023-05-27 21:27:41 +0300

    Fix server option

--- phonetic_transcriber.py
+++ phonetic_transcriber.py
@@ -245,7 +245,7 @@ if __name__ == '__main__':
     parser.add_argument('--phrase', '-p', action='append', help='input phrase to transcribe')
     parser.add_argument('--json', '-j', metavar='FILE', type=str, help='output to json file, - to stdout')
     parser.add_argument('--tsv', metavar='FILE', type=str, help='output to Tab Separated Value file, - to stdout')
-    parser.add_argument('--server', '-s', metavar='HOST:PORT', default='localhost:8080', help='run server listening on [HOST]:PORT')
+    parser.add_argument('--server', '-s', metavar='HOST:PORT', help='run server listening on [HOST]:PORT')
     parser.add_argument('word', nargs='*', type=str, help='input word to transcribe')
 
     args = parser.parse_args()
