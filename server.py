c3ab07fd5bf099f5da345d95f362ee1e91f537f9
Didzis Gosko <didzis@users.noreply.github.com>
2023-05-29 01:23:46 +0300

    HTTP server: return 500 on handler exception

--- server.py
+++ server.py
@@ -62,114 +62,120 @@ def run_server(address, transcriber, debug=False):
             try:
                 async with async_timeout(30):
                     with closing(writer):
-                        data = await reader.readuntil(b'\r\n\r\n')
-                        addr = writer.get_extra_info('peername')
-
-                        if debug:
-                            print(f'Received {data} from {addr!r}')
-
-                        request = data.decode('utf8').split('\r\n')
-
-                        m = re.match(r'([A-Z]+) ([^\s]+) HTTP\/[.0-9]+', request[0])
-                        if not m:
-                            print(f'Invalid request received {data} from {addr!r}')
-                            return
-
-                        method, path = m.groups()
-
-                        print(f'{addr!r} {method} {path}')
-
-                        headers = {}
-                        for line in request[1:]:
-                            m = re.match(r'([^:]+): ([^\r\n]+)', line)
-                            if m:
-                                key, value = m.groups()
-                                headers[key.lower()] = value    # we do not process recurring headers
-
-                        if not (path.startswith('/transcribe?') or path == '/transcribe'):
-                            write_response(writer, addr, '404 Not Found')
-                            return
-
-                        if not (method in ('GET', 'POST') and path.startswith('/transcribe?')) and not (method == 'POST' and path == '/transcribe'):
-                            write_response(writer, addr, '400 Bad Request')
-                            return
-
-                        body = int(headers.get('content-length', 0))
-
-                        if body > 0:
-                            body = await reader.read(body)
-
-                        if method == 'POST':
-                            content_type = headers.get('content-type', 'text/html')
-                            mime_type, *content_type_params = content_type.split(';')
-                            try:
-                                content_type_params = {key:value for key, value in (kv.split('=') for kv in content_type_params)}
-                            except:
-                                write_response(writer, addr, '400 Bad Request')
-                                return
-                            content_charset = content_type_params.get('charset', 'utf-8')
-                            if content_charset != 'utf-8':
-                                print(f'charset {charset} is not  supported')
-                                write_response(writer, addr, '400 Bad Request')
-                                return
-                            if mime_type in ('text', 'text/html'):
-                                text = body.decode('utf8')
-                            elif mime_type == 'application/json':
-                                body = body.decode('utf8')
-                                body = json.loads(body)
-                                text = body.get('text')
-                            else:
-                                print(f'MIME type {mime_type} is not  supported')
-                                write_response(writer, addr, '400 Bad Request')
-                                return
-                        else:
-                            text = None
-
-                        qs = parse_qs(path.split('?', 1)[1], True)
-
-                        if not qs.get('text') and not text and not qs.get('word'):
-                            write_response(writer, addr, '400 Bad Request')
-                            return
-
-                        sep = qs.get('sep', [' '])[0]
-                        unknown_sep = qs.get('unknown_sep', qs.get('usep', [sep]))[0]
-                        phoneme_sep = qs.get('phoneme_sep', qs.get('psep', [sep]))[0]
-
-                        preserve_unknown = True
-                        unknown_str = qs.get('unknown', qs.get('u', ['true']))[0]
-                        if unknown_str in '1tTyY' or unknown_str.lower() in ('true', 'yes'):
-                            preserve_unknown = True
-                        elif unknown_str in '0fFnN' or unknown_str.lower() in ('false', 'no'):
-                            preserve_unknown = False
-
-                        word = clean_text(qs.get('word', [''])[0])
-                        if text is None:
-                            text = clean_text(qs.get('text', [''])[0])
-
                         try:
-                            if word:
-                                print(f'Transcribing: {word}')
-                                result = transcriber.transcribe(word, sep)
-                            elif text:
-                                print(f'Transcribing phrase: {text}')
-                                result = transcriber.transcribeText(text, preserve_unknown=preserve_unknown, sep=phoneme_sep, unknown_sep=unknown_sep)
+                            data = await reader.readuntil(b'\r\n\r\n')
+                            addr = writer.get_extra_info('peername')
+
+                            if debug:
+                                print(f'Received {data} from {addr!r}')
+
+                            request = data.decode('utf8').split('\r\n')
+
+                            m = re.match(r'([A-Z]+) ([^\s]+) HTTP\/[.0-9]+', request[0])
+                            if not m:
+                                print(f'Invalid request received {data} from {addr!r}')
+                                return
+
+                            method, path = m.groups()
+
+                            print(f'{addr!r} {method} {path}')
+
+                            headers = {}
+                            for line in request[1:]:
+                                m = re.match(r'([^:]+): ([^\r\n]+)', line)
+                                if m:
+                                    key, value = m.groups()
+                                    headers[key.lower()] = value    # we do not process recurring headers
+
+                            if not (path.startswith('/transcribe?') or path == '/transcribe'):
+                                write_response(writer, addr, '404 Not Found')
+                                return
+
+                            if not (method in ('GET', 'POST') and path.startswith('/transcribe?')) and not (method == 'POST' and path == '/transcribe'):
+                                write_response(writer, addr, '400 Bad Request')
+                                return
+
+                            body = int(headers.get('content-length', 0))
+
+                            if body > 0:
+                                body = await reader.read(body)
+
+                            if method == 'POST':
+                                content_type = headers.get('content-type', 'text/html')
+                                mime_type, *content_type_params = content_type.split(';')
+                                try:
+                                    content_type_params = {key:value for key, value in (kv.split('=') for kv in content_type_params)}
+                                except:
+                                    write_response(writer, addr, '400 Bad Request')
+                                    return
+                                content_charset = content_type_params.get('charset', 'utf-8')
+                                if content_charset != 'utf-8':
+                                    print(f'charset {charset} is not  supported')
+                                    write_response(writer, addr, '400 Bad Request')
+                                    return
+                                if mime_type in ('text', 'text/html'):
+                                    text = body.decode('utf8')
+                                elif mime_type == 'application/json':
+                                    body = body.decode('utf8')
+                                    body = json.loads(body)
+                                    text = body.get('text')
+                                else:
+                                    print(f'MIME type {mime_type} is not  supported')
+                                    write_response(writer, addr, '400 Bad Request')
+                                    return
+                            else:
+                                text = None
+
+                            qs = parse_qs(path.split('?', 1)[1], True)
+
+                            if not qs.get('text') and not text and not qs.get('word'):
+                                write_response(writer, addr, '400 Bad Request')
+                                return
+
+                            sep = qs.get('sep', [' '])[0]
+                            unknown_sep = qs.get('unknown_sep', qs.get('usep', [sep]))[0]
+                            phoneme_sep = qs.get('phoneme_sep', qs.get('psep', [sep]))[0]
+
+                            preserve_unknown = True
+                            unknown_str = qs.get('unknown', qs.get('u', ['true']))[0]
+                            if unknown_str in '1tTyY' or unknown_str.lower() in ('true', 'yes'):
+                                preserve_unknown = True
+                            elif unknown_str in '0fFnN' or unknown_str.lower() in ('false', 'no'):
+                                preserve_unknown = False
+
+                            word = clean_text(qs.get('word', [''])[0])
+                            if text is None:
+                                text = clean_text(qs.get('text', [''])[0])
+
+                            try:
+                                if word:
+                                    print(f'Transcribing: {word}')
+                                    result = transcriber.transcribe(word, sep)
+                                elif text:
+                                    print(f'Transcribing phrase: {text}')
+                                    result = transcriber.transcribeText(text, preserve_unknown=preserve_unknown, sep=phoneme_sep, unknown_sep=unknown_sep)
+                            except Exception as e:
+                                print(traceback.format_exc())
+                                print(f'Got exception: {e}')
+                                # write_response(writer, addr, '400 Bad Request')
+                                write_response(writer, addr, '500 Internal Server Error')
+                                return
+
+                            print(f'Got result: {result}')
+                            # print(result.encode('utf8'))
+
+                            write_response(writer, addr, '200 OK', {'Content-Type': 'text/html; charset=utf-8'}, body=result)
+
+                            # writer.write(b'HTTP/1.1 200 OK\r\n')
+                            # writer.write(b'Host: localhost\r\n')
+                            # writer.write(b'Content-Length: 2\r\n')
+                            # writer.write(b'\r\n')
+                            # writer.write(b'OK')
+
                         except Exception as e:
                             print(traceback.format_exc())
-                            print(f'Got exception: {e}')
-                            # write_response(writer, addr, '400 Bad Request')
+                            print(f'{addr} Exception:', e)
                             write_response(writer, addr, '500 Internal Server Error')
-                            return
-
-                        print(f'Got result: {result}')
-                        # print(result.encode('utf8'))
-
-                        write_response(writer, addr, '200 OK', {'Content-Type': 'text/html; charset=utf-8'}, body=result)
-
-                        # writer.write(b'HTTP/1.1 200 OK\r\n')
-                        # writer.write(b'Host: localhost\r\n')
-                        # writer.write(b'Content-Length: 2\r\n')
-                        # writer.write(b'\r\n')
-                        # writer.write(b'OK')
 
             except asyncio.TimeoutError:
                 print(f'Timeout {addr}')
